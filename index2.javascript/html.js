645db61db77fa692cce8de7be000de72fd002faf
Saqlian Ashraf <saqlainashraf4741341@gmail.com>
2025-09-17 11:38:38 +0500

    Add files via upload

---
+++ index2.javascript/html.js
@@ -0,0 +1,196 @@
+let a = Number(prompt("first number"));
+console.log("first number is", a );
+let b = Number(prompt("second number"));
+console.log("second number is", b);
+let sum = a+b;
+console.log("sum is ", sum);
+
+let a = Number(prompt("first number"));
+let b = Number (prompt("second number"));
+let sum = a+b;
+console.log("the sum is", sum);
+
+let a = Number(prompt("Enter First Number")); 
+let b = Number(prompt("Enter Second Number"));
+let sum = a+b;
+let diff = a-b;
+let product = a*b;
+let quiotent = a/b;
+console.log("This sum", sum);
+console.log("This Diffrent", diff);
+console.log("This Product", product);
+console.log("This Quiotent", quiotent);
+
+let age1 = Number(prompt("Enter Age of Student 1:"));
+let age2 = Number(prompt("Enter Age of Student 2:"));
+let age3 = Number(prompt("Enter Age of Student 3:"));
+let age4 = Number(prompt("Enter Age of Student 4:"));
+let age5 = Number(prompt("Enter Age of Student 5:"));
+let age6 = Number(prompt("Enter Age of Student 6:"));
+let age7 = Number(prompt("Enter Age of Student 7:"));
+let age8 = Number (prompt("Enter Age of Student 8:"));
+let age9 = Number(prompt("Enter Age of Student 9:"));
+let age10 = Number(prompt("Enter Age of Student 10:"));
+let totel = age1 + age2 + age3 + age4 + age5 + age6 + age7 + age8 + age9 + age10;
+let avarage = totel / 10;
+console.log("totel", totel);
+console.log("avarage of totel student ages", avarage);
+
+let rent = ("200");
+let groceires = ("50");
+let entertainment = ("30");
+let totalexpenceis = rent+groceires+entertainment;
+let budget = 800;
+let remaining = totalexpenseis-budget;
+console.log("Totel Expancies", + totalexpenceis);
+console.log("Totel Budget", + budget);
+console.log("Remaining after expencies", + remaining);
+
+
+
+let rent = 200;
+let groceries = 50;
+let entertainment = 30;
+
+
+let totalExpenses = rent + groceries + entertainment;
+
+
+let budget = 500; 
+
+let remaining = budget - totalExpenses;
+
+
+console.log("Total Expenses: " + totalExpenses);        
+console.log("Budget: " + budget);                      
+console.log("Remaining after expenses: " + remaining); 
+let yourAge = Number(prompt("Your Current Age"));
+let myAge = 90;
+let futureAge = yourAge * 52;
+console.log(futureAge);
+
+
+let rent = 200;
+let groceires = 50;
+let entertainment = 30;
+let totalExpenses = rent + groceires + entertainment;
+console.log(totalExpenses);
+let budget = 500;
+let sub = budget-totalExpenses;
+console.log(sub);
+let surprise = 50;
+let sum = surprise + sub;
+console.log(sum);
+let saving = sum * 0.10;
+console.log(saving);
+let final = sum - saving;
+console.log(final);
+
+let age = Number(prompt("Enter Age"));
+
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Not Adult");
+}
+
+let marks = Number(prompt("Enter Your Marks"));
+if (marks >=45) { 
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
+
+let a = 13;
+let b = 24;
+console.log(a+b);
+ let age = Number(prompt("Your Enter age"));
+if (age>=25) {
+ console.log("True");   
+ } else{
+     console.log("False");
+}
+
+ let number = Number (prompt("Enter Select Number"));
+ if (number%2 == 0) {
+ console.log("Even");
+} else{
+     console.log("Odd"); 
+}
+let student = ["Ali", "Bhai", "Kachu", "Hijjan","jahau"];
+console.log(student);
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits[0]); 
+console.log(fruits[1]); 
+console.log(fruits[2]); 
+let startup = "javascript";
+let results = startup.slice(0,4);
+console.log(results);
+let name = "Saqlain ashraf";
+console.log(name.trimEnd());
+console.log(name.trimStart());
+console.log(name.trim());
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
